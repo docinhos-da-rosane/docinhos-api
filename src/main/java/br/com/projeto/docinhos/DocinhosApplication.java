99ce5d5583793116ca2306ba7768c72a48dae919
Rachel Pizane <rachel.pzm@gmail.com>
2026-08-09 11:27:18 -0300

    init: iniciar projeto

---
+++ src/main/java/br/com/projeto/docinhos/DocinhosApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.docinhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocinhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocinhosApplication.class, args);
+	}
+
+}
