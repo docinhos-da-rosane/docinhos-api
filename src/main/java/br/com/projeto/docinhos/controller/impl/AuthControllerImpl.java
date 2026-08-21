be8ae3b08eaba972a76ec0e78f1bac0a200704a5
Rachel Pizane <rachel.pzm@gmail.com>
2026-08-20 21:57:33 -0300

    fix: corrigir intercepção da rota de erro

--- src/main/java/br/com/projeto/docinhos/controller/impl/AuthControllerImpl.java
+++ src/main/java/br/com/projeto/docinhos/controller/impl/AuthControllerImpl.java
@@ -5,6 +5,8 @@ import br.com.projeto.docinhos.dto.AuthRequest;
 import br.com.projeto.docinhos.dto.AuthResponse;
 import br.com.projeto.docinhos.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -14,6 +16,7 @@ public class AuthControllerImpl implements AuthController {
   private final AuthService authService;
 
   @Override
+  @ResponseStatus(HttpStatus.OK)
   public AuthResponse logar(AuthRequest request) {
     return authService.logar(request);
   }
--- src/main/java/br/com/projeto/docinhos/security/AuthenticationEntryPointImpl.java
+++ src/main/java/br/com/projeto/docinhos/security/AuthenticationEntryPointImpl.java
@@ -1,22 +1,38 @@
 package br.com.projeto.docinhos.security;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
   private final SecurityErrorResponseService errorResponseService;
+  private final AuthenticationEntryPoint defaultEntryPoint = new Http403ForbiddenEntryPoint();
 
   @Override
   public void commence(
       HttpServletRequest request,
       HttpServletResponse response,
-      AuthenticationException authException) {
-    errorResponseService.escreverErroCredenciaisInvalidas(response, request);
+      AuthenticationException authException)
+      throws IOException, ServletException {
+
+    if (deveUsarErroCustomizado(authException)) {
+      errorResponseService.escreverErroCredenciaisInvalidas(response, request);
+      return;
+    }
+
+    defaultEntryPoint.commence(request, response, authException);
+  }
+
+  private boolean deveUsarErroCustomizado(AuthenticationException authException) {
+    return authException instanceof InternalAuthenticationServiceException;
   }
 }
