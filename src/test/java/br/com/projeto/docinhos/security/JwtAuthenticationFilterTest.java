0b728f3c4df83948ce283f554f47bfa3a216ab81
Rachel Pizane <rachel.pzm@gmail.com>
2026-08-23 12:20:28 -0300

    fix: formatar checkstyle; deixar dados do jwt em segredo

--- src/test/java/br/com/projeto/docinhos/security/JwtAuthenticationFilterTest.java
+++ src/test/java/br/com/projeto/docinhos/security/JwtAuthenticationFilterTest.java
@@ -1,6 +1,5 @@
 package br.com.projeto.docinhos.security;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
